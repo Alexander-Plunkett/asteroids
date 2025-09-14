775a59ed8a2974d4f6d90ffbc577898835f6959d
Alexander-Plunkett <alexander.b.plunkett@gmail.com>
2025-09-14 07:57:56 +0100

    added player shooting

--- circleshape.py
+++ circleshape.py
@@ -21,5 +21,5 @@ class CircleShape(pygame.sprite.Sprite):
         # sub-classes must override
         pass
 
-    def isColliding(self, other):
+    def is_colliding(self, other):
         return self.position.distance_to(other.position) <= self.radius + other.radius
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,10 +13,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -31,7 +34,7 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
-            if player.isColliding(asteroid):
+            if asteroid.is_colliding(player):
                 print("Game over!")
                 sys.exit()
 
