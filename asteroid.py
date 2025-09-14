ed3f3af490c50b01df546be3a8212e241f7c4ff9
Alexander-Plunkett <alexander.b.plunkett@gmail.com>
2025-09-14 10:33:46 +0100

    add asteroid splitting when larger asteroid is shot, guided portion of project complete

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -7,6 +9,23 @@ class Asteroid(CircleShape):
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+    
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        angle = random.uniform(20, 50)
+        new_velocity_l = self.velocity.rotate(-angle)
+        new_velocity_r = self.velocity.rotate(angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new_asteroid_l = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_r = Asteroid(self.position.x, self.position.y, new_radius)
+
+        new_asteroid_l.velocity = new_velocity_l * 1.2
+        new_asteroid_r.velocity = new_velocity_r * 1.2
 
     def update(self, dt):
         self.position += self.velocity * dt
